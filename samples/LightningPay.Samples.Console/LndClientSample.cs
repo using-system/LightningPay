97b9724d17b87bfe755a1d7a523c95ab5cab610a
Matthieu Nicolescu <mnicolescu@gmail.com>
2021-06-19 22:21:15 +0200

    Remove references about Microsoft.Extensions.Options

--- samples/LightningPay.Samples.Console/LndClientSample.cs
+++ samples/LightningPay.Samples.Console/LndClientSample.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-using Microsoft.Extensions.Options;
 
-using LightningPay.Core;
-using LightningPay.Core.Clients.Lnd;
+using LightningPay.Clients.Lnd;
 
 namespace LightningPay.Samples.Console
 {
@@ -12,26 +10,14 @@ namespace LightningPay.Samples.Console
     {
         public async override Task Execute()
         {
-            var client = new LndClient(new System.Net.Http.HttpClient(), new LndOptionsWrapper(new LndOptions()
+            var client = new LndClient(new System.Net.Http.HttpClient(), new LndOptions()
             {
                 BaseUri = new Uri("http://localhost:42802/")
-            }));
+            });
 
             var invoice = await client.CreateInvoice(LightMoney.MilliSatoshis(1000), "Test", TimeSpan.FromMinutes(5));
 
             System.Console.WriteLine(invoice.Id);
-        }
-
-        public class LndOptionsWrapper : IOptions<LndOptions>
-        {
-            private readonly LndOptions options;
-
-            public LndOptionsWrapper(LndOptions options)
-            {
-                this.options = options;
-            }
-
-            public LndOptions Value => this.options;
         }
     }
 }
