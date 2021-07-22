64d1d87c753fb0ea777d50933fed855dc76b8031
Matthieu Nicolescu <mnicolescu@gmail.com>
2021-07-22 14:36:59 +0200

    Add first LndListener

---
+++ src/LightningPay/Clients/Lnd/LndListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LightningPay.Clients.Lnd
+{
+    public class LndListener : ILightningListener
+    {
+        public void Subscribe<TEvent, THandler>()
+            where TEvent : LightningEvent
+            where THandler : ILightningEventHandler<TEvent>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : LightningEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe<TEvent, THandler>()
+            where TEvent : LightningEvent
+            where THandler : ILightningEventHandler<TEvent>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
